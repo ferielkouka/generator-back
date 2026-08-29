a8777b723c0ecdf942408e9b02c93e6f8de609a0
ferielkouka <ferieelkoukaa@gmail.com>
2026-08-29 03:41:09 +0200

    Generate/update Laravel backend for article

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     protected $table = 'articles';
-    protected $fillable = ['name', 'size', 'price'];
+    protected $fillable = ['title', 'content'];
 }
