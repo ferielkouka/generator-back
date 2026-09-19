09654aeb6af8be21605221d04512e1123f22bf6d
ferielkouka <ferieelkoukaa@gmail.com>
2026-09-19 13:06:52 +0200

    Generate/update Laravel backend for fournisseur

--- app/Http/Controllers/FournisseurController.php
+++ app/Http/Controllers/FournisseurController.php
@@ -1,7 +1,7 @@
 <?php
-namespace App\\Http\\Controllers;
-use Illuminate\\Http\\Request;
-use App\\Models\\Fournisseur;
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Fournisseur;
 
 class FournisseurController extends Controller
 {
