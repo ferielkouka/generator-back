73c2e09219a5d704201963e4d48a44e1f5a1a3f8
ferielkouka <ferieelkoukaa@gmail.com>
2026-09-19 13:20:52 +0200

    Generate/update Laravel backend for fournisseur

--- app/Http/Controllers/FournisseurController.php
+++ app/Http/Controllers/FournisseurController.php
@@ -5,27 +5,27 @@ use App\Models\Fournisseur;
 
 class FournisseurController extends Controller
 {
-  public function index()
-  {
-    return response()->json(Fournisseur::all());
-  }
+    public function index()
+    {
+        return response()->json(Fournisseur::all());
+    }
 
-  public function store(Request $request)
-  {
-    $item = Fournisseur::create($request->all());
-    return response()->json($item, 201);
-  }
+    public function store(Request $request)
+    {
+        $item = Fournisseur::create($request->all());
+        return response()->json($item, 201);
+    }
 
-  public function update(Request $request, $id)
-  {
-    $item = Fournisseur::findOrFail($id);
-    $item->update($request->all());
-    return response()->json($item);
-  }
+    public function update(Request $request, $id)
+    {
+        $item = Fournisseur::findOrFail($id);
+        $item->update($request->all());
+        return response()->json($item);
+    }
 
-  public function destroy($id)
-  {
-    Fournisseur::destroy($id);
-    return response()->json(['message' => 'Deleted']);
-  }
+    public function destroy($id)
+    {
+        Fournisseur::destroy($id);
+        return response()->json(['message' => 'Deleted']);
+    }
 }
